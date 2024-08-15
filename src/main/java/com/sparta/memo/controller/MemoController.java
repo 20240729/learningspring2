018283e598a5c4c10b964431c97854ef7bcc9bf5
20240729 <yoohyeonsik@gmail.com>
2024-08-15 20:42:52 +0900

    메모 생성하기 api 하나가 만들어 졌다고 함...(근데잘모르겠음..)17강 2045

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -22,10 +22,20 @@ public class MemoController {
 
         // Memo Max ID Check (중복체크?)
         Long maxId = memoList.size() > 0 ? Collections.max(memoList.keySet()) + 1 : 1;
+        memo.setId(maxId); // 이걸로 번호를 넣음
         // 리스트의 크기가 0보다 크면 Long maxId가 키 중에서 제일 큰 것(모든 키의 집합 반환 메서드를
         // 입력된 배열 중에서 가장 큰 요소를 반환하는 메서드에 넣음)에 +1한 값이 됨.
         // 리스트의 크기가 0이면 1이 됨
         // 복습 : 삼항연산자 =>    조건 ? 참이면 실행할것 : 거짓이면 실행할것
+
+        // DB 저장
+        memoList.put(memo.getId(), memo);
+
+        // Entity -> ResponseDto
+        MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
+
+        return memoResponseDto;
+
 
     }
 
