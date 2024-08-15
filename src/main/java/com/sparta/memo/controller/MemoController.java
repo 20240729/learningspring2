6eefe0ae195d9b358d54e5a7bee0f5d8e557b1c7
20240729 <yoohyeonsik@gmail.com>
2024-08-15 20:35:10 +0900

    중복체크

---
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -0,0 +1,36 @@
+package com.sparta.memo.controller;
+
+import com.sparta.memo.dto.MemoRequestDto;
+import com.sparta.memo.dto.MemoResponseDto;
+import com.sparta.memo.entity.Memo;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class MemoController {
+
+    private final Map<Long, Memo> memoList = new HashMap<>();
+
+    @PostMapping("memos")
+    public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto){
+        // RequestDto -> Entity (데이터는 데이터베이스와 소통하는 엔티티에 들어간다.)
+        Memo memo = new Memo(requestDto);
+
+        // Memo Max ID Check (중복체크?)
+        Long maxId = memoList.size() > 0 ? Collections.max(memoList.keySet()) + 1 : 1;
+        // 리스트의 크기가 0보다 크면 Long maxId가 키 중에서 제일 큰 것(모든 키의 집합 반환 메서드를
+        // 입력된 배열 중에서 가장 큰 요소를 반환하는 메서드에 넣음)에 +1한 값이 됨.
+        // 리스트의 크기가 0이면 1이 됨
+        // 복습 : 삼항연산자 =>   조건 ? 참이면 실행할것 : 거짓이면 실행할것
+
+    }
+
+
+
+
+
+}
